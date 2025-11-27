e54dd1ac8013453a6c2275b6cd9b31e9104adaee
Darshan.J <darshan.j@sapiens.com>
2025-11-27 06:50:58 +0530

    added new file for library

---
+++ vars/reusablePipeline.groovy
@@ -0,0 +1,38 @@
+def call (String gitUrl) {
+    pipeline {
+        agent { label "qa-server"}
+        stages {
+            stage('Checkout') {
+                steps {
+                    checkout([
+                            $class: 'GitSCM',
+                            branches: [[name: '*/main']],
+                            userRemoteConfigs: [
+                                    url: gitUrl,
+                                    credentialsId: 'github-pass'
+                            ]
+                    ])
+                }
+            }
+            stage('Build') {
+                steps {
+                    sh 'mvn clean install'
+                }
+            }
+            stage('Test') {
+                steps {
+                    sh 'mvn test'
+                }
+            }
+            stage('Deploy to Artifactory') {
+                steps {
+                    configFileProvider([configFile(fileID: "da662eac-7a53-4268-beec-69db67ad3a65", variable: 'MAVEN_SETTINGS')]) {
+                        sh 'mvn deploy -s $MAVEN_SETTINGS'
+                    }
+
+                }
+            }
+
+        }
+    }
+}
