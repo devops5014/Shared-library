d3014087da8ec11124902cace3d66acfaadabfa7
Darshan.J <darshan.j@sapiens.com>
2025-11-27 07:03:26 +0530

    added new file for library

--- vars/reusablePipeline.groovy
+++ vars/reusablePipeline.groovy
@@ -1,38 +1,48 @@
-def call (String gitUrl, String gitToken) {
+def call(String gitUrl, String gitToken) {
     pipeline {
-        agent { label "qa-server"}
+        agent { label 'qa-server' }
+
         stages {
             stage('Checkout') {
                 steps {
-                    checkout([
-                            $class: 'GitSCM',
-                            branches: [[name: '*/main']],
-                            userRemoteConfigs: [
-                                    url: gitUrl,
-                                    credentialsId: gitToken
-                            ]
-                    ])
+                    script {
+                        echo "Cloning repository: ${gitUrl}"
+                        checkout([
+                                $class: 'GitSCM',
+                                branches: [[name: '*/main']],
+                                userRemoteConfigs: [[
+                                                            url: gitUrl,
+                                                            credentialsId: gitToken
+                                                    ]]
+                        ])
+                    }
                 }
             }
+
             stage('Build') {
                 steps {
                     sh 'mvn clean install'
                 }
             }
+
             stage('Test') {
                 steps {
                     sh 'mvn test'
                 }
             }
+
             stage('Deploy to Artifactory') {
                 steps {
-                    configFileProvider([configFile(fileID: "da662eac-7a53-4268-beec-69db67ad3a65", variable: 'MAVEN_SETTINGS')]) {
+                    configFileProvider([
+                            configFile(
+                                    fileId: 'da662eac-7a53-4268-beec-69db67ad3a65',
+                                    variable: 'MAVEN_SETTINGS'
+                            )
+                    ]) {
                         sh 'mvn deploy -s $MAVEN_SETTINGS'
                     }
-
                 }
             }
-
         }
     }
 }
