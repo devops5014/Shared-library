9bf06c9a539f4e0011423cd0e9f13a21c6238f13
Darshan.J <darshan.j@sapiens.com>
2025-11-27 05:39:06 +0530

    rem

---
+++ vars/reusablePipeline.groovy
@@ -0,0 +1,46 @@
+def call(String gitUrl, String gitToken) {
+    pipeline {
+        agent { label 'qa-server' }
+
+        stages {
+            stage('Checkout') {
+                steps {
+                    echo "Cloning repository: ${gitUrl}"
+                    checkout([
+                            $class: 'GitSCM',
+                            branches: [[name: '*/main']],
+                            userRemoteConfigs: [[
+                                                        url: gitUrl,
+                                                        credentialsId: gitToken
+                                                ]]
+                    ])
+                }
+            }
+
+            stage('Build') {
+                steps {
+                    sh 'mvn clean install'
+                }
+            }
+
+            stage('Test') {
+                steps {
+                    sh 'mvn test'
+                }
+            }
+
+            stage('Deploy to Artifactory') {
+                steps {
+                    configFileProvider([
+                            configFile(
+                                    fileId: 'da662eac-7a53-4268-beec-69db67ad3a65',
+                                    variable: 'MAVEN_SETTINGS'
+                            )
+                    ]) {
+                        sh 'mvn deploy -s $MAVEN_SETTINGS'
+                    }
+                }
+            }
+        }
+    }
+}
